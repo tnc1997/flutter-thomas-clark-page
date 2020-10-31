82bdcf33433e4ff862a1dc91d2a81b2edce8b5e6
Thomas Clark <tnc1997@virginmedia.com>
2020-10-31 20:06:15 +0000

    feat: add settings page (#15)

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:thomas_clark/route_information_parsers/app_route_information_parser.dart';
 import 'package:thomas_clark/router_delegates/app_router_delegate.dart';
 import 'package:thomas_clark/states/router_state.dart';
+import 'package:thomas_clark/states/theme_state.dart';
 
 void main() {
   runApp(App());
@@ -16,41 +17,50 @@ class _AppState extends State<App> {
   final _routeInformationParser = AppRouteInformationParser();
 
   AppRouterDelegate _routerDelegate;
-  RouterStateData _notifier;
+  RouterStateData _routerStateData;
+  ThemeStateData _themeStateData;
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routeInformationParser: _routeInformationParser,
-      routerDelegate: _routerDelegate,
-      title: 'Thomas Clark',
-      theme: ThemeData.from(
-        colorScheme: ColorScheme.light(
-          primary: Colors.red,
-          primaryVariant: Colors.red[700],
-          secondary: Colors.red,
-          secondaryVariant: Colors.red[700],
-          surface: Colors.white,
-          background: Colors.grey[50],
-          onPrimary: Colors.black,
-          onSecondary: Colors.black,
-          onSurface: Colors.black,
-          onBackground: Colors.black,
-        ),
-      ),
-      darkTheme: ThemeData.from(
-        colorScheme: ColorScheme.dark(
-          primary: Colors.red[200],
-          primaryVariant: Colors.red[700],
-          secondary: Colors.red[200],
-          secondaryVariant: Colors.red[700],
-          surface: Colors.grey[850],
-          background: Colors.grey[900],
-          onPrimary: Colors.black,
-          onSecondary: Colors.black,
-          onSurface: Colors.white,
-          onBackground: Colors.white,
-        ),
+    return ThemeState(
+      notifier: _themeStateData,
+      child: Builder(
+        builder: (context) {
+          return MaterialApp.router(
+            routeInformationParser: _routeInformationParser,
+            routerDelegate: _routerDelegate,
+            title: 'Thomas Clark',
+            theme: ThemeData.from(
+              colorScheme: ColorScheme.light(
+                primary: Colors.red,
+                primaryVariant: Colors.red[700],
+                secondary: Colors.red,
+                secondaryVariant: Colors.red[700],
+                surface: Colors.white,
+                background: Colors.grey[50],
+                onPrimary: Colors.black,
+                onSecondary: Colors.black,
+                onSurface: Colors.black,
+                onBackground: Colors.black,
+              ),
+            ),
+            darkTheme: ThemeData.from(
+              colorScheme: ColorScheme.dark(
+                primary: Colors.red[200],
+                primaryVariant: Colors.red[700],
+                secondary: Colors.red[200],
+                secondaryVariant: Colors.red[700],
+                surface: Colors.grey[850],
+                background: Colors.grey[900],
+                onPrimary: Colors.black,
+                onSecondary: Colors.black,
+                onSurface: Colors.white,
+                onBackground: Colors.white,
+              ),
+            ),
+            themeMode: ThemeState.of(context).themeMode,
+          );
+        },
       ),
     );
   }
@@ -58,16 +68,20 @@ class _AppState extends State<App> {
   @override
   void dispose() {
     _routerDelegate.dispose();
-    _notifier.dispose();
+    _routerStateData.dispose();
+    _themeStateData.dispose();
     super.dispose();
   }
 
   @override
   void initState() {
     super.initState();
-    _notifier = RouterStateData();
+    _routerStateData = RouterStateData();
     _routerDelegate = AppRouterDelegate(
-      notifier: _notifier,
+      notifier: _routerStateData,
+    );
+    _themeStateData = ThemeStateData(
+      themeMode: ThemeMode.system,
     );
   }
 }
---
+++ lib/states/theme_state.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class ThemeState extends InheritedNotifier<ThemeStateData> {
+  const ThemeState({
+    Key key,
+    ThemeStateData notifier,
+    @required Widget child,
+  }) : super(
+          key: key,
+          notifier: notifier,
+          child: child,
+        );
+
+  static ThemeStateData of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<ThemeState>().notifier;
+  }
+}
+
+class ThemeStateData extends ChangeNotifier {
+  ThemeMode _themeMode;
+
+  ThemeStateData({
+    ThemeMode themeMode,
+  }) : _themeMode = themeMode;
+
+  ThemeMode get themeMode => _themeMode;
+
+  set themeMode(ThemeMode value) {
+    if (value != _themeMode) {
+      _themeMode = value;
+      notifyListeners();
+    }
+  }
+}
