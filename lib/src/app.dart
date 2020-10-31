9af22aedcf979771f42ed2fcc6023982c05eb2b7
Thomas Clark <tnc1997@virginmedia.com>
2020-10-31 07:13:10 +0000

    feat: configure router

---
+++ lib/src/app.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:thomas_clark/src/route_information_parsers/app_route_information_parser.dart';
+import 'package:thomas_clark/src/router_delegates/app_router_delegate.dart';
+import 'package:thomas_clark/src/states/router_state.dart';
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  final _routeInformationParser = AppRouteInformationParser();
+
+  AppRouterDelegate _routerDelegate;
+  RouterStateData _notifier;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routeInformationParser: _routeInformationParser,
+      routerDelegate: _routerDelegate,
+      title: 'Flutter Demo',
+    );
+  }
+
+  @override
+  void dispose() {
+    _routerDelegate.dispose();
+    _notifier.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _notifier = RouterStateData();
+    _routerDelegate = AppRouterDelegate(
+      notifier: _notifier,
+    );
+  }
+}
