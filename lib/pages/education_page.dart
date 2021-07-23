5505873cefa5923698aea83131ec9856d7f2ff98
Thomas Clark <tnc1997@virginmedia.com>
2021-07-23 07:42:55 +0100

    chore: align with curriculum vitae

--- lib/pages/education_page.dart
+++ lib/pages/education_page.dart
@@ -17,19 +17,19 @@ class EducationPage extends StatelessWidget {
         children: [
           AppCard(
             title: 'Computing BSc (Hons) at University of Gloucestershire',
-            subtitle: 'September 2015 - July 2019',
+            subtitle: '2015 - 2019',
             content:
                 'I graduated with a first class degree with honours after studying modules including Introduction To Website Development, Agile Methods, Mobile Application Development and Advanced Database Systems.',
           ),
           AppCard(
             title: 'Advanced Level at St. Brendan\'s Sixth Form College',
-            subtitle: 'September 2013 - July 2015',
+            subtitle: '2013 - 2015',
             content:
                 'Applied IT (B), Computing (C), Chemistry (C), Mathematics (D).',
           ),
           AppCard(
             title: 'GCSE at The Grange School',
-            subtitle: 'September 2008 - July 2013',
+            subtitle: '2008 - 2013',
             content:
                 '9 Grade A-C, IT Diploma, English Baccalaureate, Functional Skills.',
           ),
